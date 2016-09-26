a399566f6c854b2b4ce2c6b7af83527090d73dde
Joel Baticz <joel.baticz@gmail.com>
2016-09-26 20:17:12 +1000

    Add files via upload

--- src/Demo/Models/Category.cs
+++ src/Demo/Models/Category.cs
@@ -1,1 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
+namespace Demo.Models
+{
+    public class Category
+    {
+        //class name + Id will be the PK in the DB
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        //relationship
+        public List<Product> Products { get; set; }
+
+    }
+}
---
+++ src/Demo/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; }
+        public double Price { get; set; }
+
+        //relationship
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        
+
+    }
+}
