0e45ee896f20be137797028d6e115bc0077cf9d4
Joel Baticz <joel.baticz@gmail.com>
2016-09-26 20:16:06 +1000

    Add files via upload

--- src/Demo/Migrations/20160826041344_addProductDatabaseMigration2.Designer.cs
+++ src/Demo/Migrations/20160826041344_addProductDatabaseMigration2.Designer.cs
@@ -8,9 +8,10 @@ using Demo.Services;
 namespace Demo.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160826041344_addProductDatabaseMigration2")]
+    partial class addProductDatabaseMigration2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.0-rtm-21431")
---
+++ src/Demo/Migrations/20160826041344_addProductDatabaseMigration2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Demo.Migrations
+{
+    public partial class addProductDatabaseMigration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TblProduct",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CategoryId = table.Column<int>(nullable: false),
+                    Details = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TblProduct", x => x.ProductId);
+                    table.ForeignKey(
+                        name: "FK_TblProduct_TblCategory_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "TblCategory",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TblProduct_CategoryId",
+                table: "TblProduct",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TblProduct");
+        }
+    }
+}
